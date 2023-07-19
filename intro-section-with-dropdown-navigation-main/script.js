f492886a3308e9a7ea927d909f2977cb9aed7708
Vaishnav Ghangale <107029372+vaishnav196@users.noreply.github.com>
2023-07-19 10:52:36 +0530

    Add files via upload

---
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -0,0 +1,10 @@
+let company = document.getElementById("company");
+let feature = document.getElementById("features");
+function togglef() {
+  feature.classList.toggle("openmenu");
+ 
+}
+
+function togglec() {
+  company.classList.toggle("openmenu");
+}
